940541625fb2405327567091f29d8aef5f054fa4
kyori19 <kyori.dis@gmail.com>
2019-02-21 22:57:25 +0900

    ゲンシジン トゥート スル

--- api/v1/genshijin.js
+++ api/v1/genshijin.js
@@ -2,7 +2,7 @@ var genshijin = require('../../lib/genshijin');
 
 module.exports = {
   post : function(req, res) {
-    genshijin(req.body.status);
-    res.send();
+    genshijin(req.body.status, req.body.domain, req.body.access_token);
+    res.send("Execution started.");
   }
 }
