cd2e7c15fa0a4e7b89faafd79921d2a1e498a8bc
kyori19 <kyori.dis@gmail.com>
2019-03-26 22:08:25 +0900

    パーセント エンコード デコード スル

--- api/v1/genshijin.js
+++ api/v1/genshijin.js
@@ -2,7 +2,7 @@ var genshijin = require('../../lib/genshijin');
 
 module.exports = {
   post : function(req, res) {
-    genshijin(req.query.status, req.query.domain, req.query.access_token);
+    genshijin(decodeURIComponent(req.query.status), req.query.domain, req.query.access_token);
     res.send("Execution started.");
   }
 }
